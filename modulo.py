139faefa12335486482ae16b0ba0e97e1dd415eb
luisalvs <luisalvst@gmail.com>
2025-09-15 22:11:00 -0300

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -5,22 +5,28 @@ LISTA_VEICULOS_SEMINOVOS = []
 
 class Carro:
     # Simula dados do veículo
-    def __init__(self, modelo, marca, ano):
-        self.placa = str
+    def __init__(self, placa, modelo, marca, ano):
+        self.placa = placa
         self.modelo = modelo
         self.marca = marca
         self.ano = ano
-        self.hodometro = 0
-
-    def cadastrar_veiculo(self):
-        placa = input('Placa: ')
-        LISTA_VEICULOS_NOVOS.append(placa)
-        print(LISTA_VEICULOS_NOVOS)
+        self.odometro = 0
 
     def exibir_veiculo(self):
-        print(
-            f'Modelo: {self.modelo} \nMarca: {self.marca} \nAno: {self.ano}')
+        return f'Placa: {self.placa} \nModelo: {self.modelo} \nMarca: {self.marca} \nAno: {self.ano} \nOdometro: {self.odometro}'
+    
+    def incrementar_odometro(self, milhas):
+            self.odometro += milhas
+
+    def atualizar_odometro(self, milhas):
+        if milhas > self.odometro:
+            self.odometro = milhas
+        else:
+             print('Odômetro não pode ser menor que o valor')
 
 
-veiculo1 = Carro('Civic', 'Honda', '2020')
-veiculo1.cadastrar_veiculo()
+
+user1 = Carro('IQI8593', 'Civic', 'Honda', 2020)
+
+user1.add_odometro(100)
+user1.exibir_veiculo()
