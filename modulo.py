4281587252dea6758d306fe18d46754fc6f9f0d3
luisalvs <luisalvst@gmail.com>
2025-09-16 10:10:08 -0300

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -5,22 +5,22 @@ LISTA_VEICULOS_SEMINOVOS = []
 
 class Carro:
     # Simula dados do veículo
-    def __init__(self, modelo, marca, ano):
-        self.placa = str
-        self.modelo = modelo
-        self.marca = marca
-        self.ano = ano
+    def __init__(self):
+        self.placa = input('Placa: ')
+        self.modelo = input('Modelo: ')
+        self.marca = input('Marca: ')
+        self.ano = int(input('Ano: '))
         self.hodometro = 0
 
     def cadastrar_veiculo(self):
-        placa = input('Placa: ')
-        LISTA_VEICULOS_NOVOS.append(placa)
-        print(LISTA_VEICULOS_NOVOS)
+        LISTA_VEICULOS_NOVOS.append(
+            {'Placa': self.placa, 'Modelo': self.modelo, 'Marca': self.marca, 'Ano': self.ano})
 
     def exibir_veiculo(self):
         print(
-            f'Modelo: {self.modelo} \nMarca: {self.marca} \nAno: {self.ano}')
+            f'Placa: {self.placa} \nModelo: {self.modelo} \nMarca: {self.marca} \nAno: {self.ano}')
 
 
-veiculo1 = Carro('Civic', 'Honda', '2020')
+veiculo1 = Carro()
 veiculo1.cadastrar_veiculo()
+veiculo1.exibir_veiculo()
